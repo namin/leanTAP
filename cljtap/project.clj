ae4819b3e54b64bc0d1510815bfba8534cc4f162
Nada Amin <namin@alum.mit.edu>
2013-03-14 00:10:57 +0100

    use core.logic release

--- cljtap/project.clj
+++ cljtap/project.clj
@@ -2,5 +2,5 @@
   :description "A Declarative Theorem Prover for First-Order Classical Logic"
   :url "https://github.com/namin/leanTAP"
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojure/core.logic "0.8.0-rc2"]
+                 [org.clojure/core.logic "0.8.0-rc3"]
                  [org.clojure/core.match "0.2.0-alpha12"]])
